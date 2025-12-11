7e371ebbcffd2b62c1b606fa87ec504fe2b5976e
SantosK145 <162088930+SantosK145@users.noreply.github.com>
2025-12-11 14:37:54 -0700

    Delete infra directory

--- infra/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "api_url" {
-  value = ""
-}
-
-output "database_url" {
-  value = "postgresql://santos:v3hQtpRGz8tVPByCV4IJB8CjWdlo2tHC@dpg-d4t7kdur433s7388cs00-a.oregon-postgres.render.com/docker_i9jo"
-}
